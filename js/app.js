b6be47f32e562d63eb7a36725e5e1eb6b4f5e440
SamuelLeiva <leiva20152@gmail.com>
2025-09-21 15:04:21 -0500

    Logro 1: reemplazo de palabras en negrita y palabras en itálica.

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ generateButton.addEventListener("click", () => {
 
 // Evento al hacer clic en el botón de cambiar estilo de encabezados
 contrastButton.addEventListener("click", () => {
-    toggleHeadingStyles(preview);
+  toggleHeadingStyles(preview);
 });
 
 // Función para convertir markdown a HTML
@@ -33,6 +33,11 @@ function convertMarkdownToHTML(markdown) {
     '<h3 class="text-xl font-bold mt-2 mb-1">$1</h3>'
   );
 
+  // Reemplazar negrita(** y __) con <strong>
+  result = result.replace(/\*\*(.*?)\*\*|__(.*?)__/g, "<strong>$1$2</strong>");
+  // Reemplazar itálica(* y _) con <em>
+  result = result.replace(/\*(.*?)\*|_(.*?)_/g, "<em>$1$2</em>");
+
   // Reemplazar ítems de listas individuales con etiquetas li estilizadas
   result = result.replace(/^\s*(-|\*)\s*(.*)$/gm, '<li class="my-1">$2</li>');
 
@@ -48,27 +53,28 @@ function convertMarkdownToHTML(markdown) {
 // Función para modificar los encabezados a un estilo diferente (color y tamaño)
 // Color azul y grande -> Color verde y mediano, y viceversa
 function toggleHeadingStyles(container) {
-    const headings = container.querySelectorAll("h1, h2, h3");
-    headings.forEach((heading) => {
-        // si el encabezado es azul, cambiar a verde, y viceversa
-        if (heading.classList.contains("text-blue-600")) {
-            heading.classList.remove("text-blue-600");
-            heading.classList.add("text-green-600");
-        } else {
-            heading.classList.remove("text-green-600");
-            heading.classList.add("text-blue-600");
-        }
+  const headings = container.querySelectorAll("h1, h2, h3");
+  headings.forEach((heading) => {
+    // si el encabezado es azul, cambiar a verde, y viceversa
+    if (heading.classList.contains("text-blue-600")) {
+      heading.classList.remove("text-blue-600");
+      heading.classList.add("text-green-600");
+    } else {
+      heading.classList.remove("text-green-600");
+      heading.classList.add("text-blue-600");
+    }
 
-        // Alternar tamaños de fuente
-        if (heading.tagName === "H1") {
-            heading.classList.toggle("text-3xl");
-            heading.classList.toggle("text-4xl");
-        } else if (heading.tagName === "H2") {
-            heading.classList.toggle("text-2xl");
-            heading.classList.toggle("text-3xl");
-        } else if (heading.tagName === "H3") {
-            heading.classList.toggle("text-xl");
-            heading.classList.toggle("text-2xl");
-        }
-    });
+    // Alternar tamaños de fuente
+    // Usamos toggle, esta función agrega la clase si no está, y la quita si está
+    if (heading.tagName === "H1") {
+      heading.classList.toggle("text-3xl");
+      heading.classList.toggle("text-4xl");
+    } else if (heading.tagName === "H2") {
+      heading.classList.toggle("text-2xl");
+      heading.classList.toggle("text-3xl");
+    } else if (heading.tagName === "H3") {
+      heading.classList.toggle("text-xl");
+      heading.classList.toggle("text-2xl");
+    }
+  });
 }
